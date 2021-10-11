bff9f73afdd4c73c0f793b3406ad262bb4298c5a
nhhoang0104 <nhhoang010420@gmail.com>
2021-10-11 23:09:43 +0700

    fix layout and add animation, js for scroll

---
+++ js/scroll.js
@@ -0,0 +1,9 @@
+window.onscroll = function () {
+  var doc = document.documentElement;
+  var top = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
+  if (top !== 0) {
+    $("#navbar").addClass("scroll");
+  } else {
+    $("#navbar").removeClass("scroll");
+  }
+};
