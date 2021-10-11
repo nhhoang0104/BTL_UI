2a8b73eb65f89ce79d0cbba4f1d7cc2e6610cd9c
nhhoang0104 <nhhoang010420@gmail.com>
2021-10-11 23:13:22 +0700

    up

--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,11 @@
+/**
+ * onscroll -> change animation, style navbar(boxShadow, transition)
+ */
+
 window.onscroll = function () {
   var doc = document.documentElement;
   var top = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
+
   if (top !== 0) {
     $("#navbar").addClass("scroll");
   } else {
